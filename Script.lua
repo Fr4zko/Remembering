b3f9831874b7ee3be8842ab45e474e032c989a6d
Fr4zko <91815132+Fr4zko@users.noreply.github.com>
2025-08-28 15:44:35 -0300

    Create Script.lua
    Adicionei o script.

---
+++ Script.lua
@@ -0,0 +1,10 @@
+local Nome = "Douglas"
+local Nivel = 15
+local Raridade = " Exotico"
+local Tipo = "Sua raridade é:"
+
+print("Name: " .. Nome .. " /" .. " Nivel: " .. Nivel)
+
+wait(5)
+
+print(Tipo .. Raridade)
